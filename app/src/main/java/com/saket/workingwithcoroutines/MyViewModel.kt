bf96ac4923c597e5185096c768252c90972f46a4
Saket Shriwas <saket.shriwas@volvocars.com>
2020-10-13 06:46:55 +0200

    coroutine main thread operation does not affect UI?

--- app/src/main/java/com/saket/workingwithcoroutines/MyViewModel.kt
+++ app/src/main/java/com/saket/workingwithcoroutines/MyViewModel.kt
@@ -18,7 +18,13 @@ class MyViewModel : ViewModel() {
         //Any coroutine launched in this scope is automatically cancelled when the viewmodel is cleared...
         viewModelScope.launch {
             //Executes on main thread by default...
+            //But this does not seem to affect the UI. Which is still responsive?
+            //The answer to this can be found here -
             //https://stackoverflow.com/questions/60273519/how-can-coroutinescopejobdispatchers-main-run-on-the-main-ui-thread
+            //Basically, the below code posts messages to the event loop of the main thread.
+            //Also Coroutines are non-blocking, which means they allow other operations to
+            //continue to execute while they are suspended. Once the message is executed by the
+            //looper in the main thread, the coroutine resumes its execution.
             Log.v("MyViewModel", "Launch viewmodelscope coroutine on thread - " + Thread.currentThread().name)
             for (i in 1..20) {
                 delay(1000)
